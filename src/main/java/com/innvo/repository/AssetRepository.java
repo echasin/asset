59472e09d4072f52d42953d45b92146daf39c158
Eric Chasiin <Eric Chasiin@MSI>
2017-05-22 00:47:41 -0400

    Ugradeto 4.5.1

--- src/main/java/com/innvo/repository/AssetRepository.java
+++ src/main/java/com/innvo/repository/AssetRepository.java
@@ -1,15 +1,16 @@
 package com.innvo.repository;
 
 import com.innvo.domain.Asset;
+import org.springframework.stereotype.Repository;
 
 import org.springframework.data.jpa.repository.*;
 
-import java.util.List;
 
 /**
  * Spring Data JPA repository for the Asset entity.
  */
 @SuppressWarnings("unused")
+@Repository
 public interface AssetRepository extends JpaRepository<Asset,Long> {
 
 }
